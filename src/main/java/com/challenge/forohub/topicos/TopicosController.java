6a6a7b868a9b3f567d8fa3d5d55169a6ee795f96
Jhon Saca <jhonsa03@gmail.com>
2025-01-22 00:26:21 -0500

    Añadiendo los Mappings para los métodos a utilizar

---
+++ src/main/java/com/challenge/forohub/topicos/TopicosController.java
@@ -0,0 +1,29 @@
+package com.challenge.forohub.topicos;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/topicos")
+public class TopicosController {
+
+    @GetMapping
+    public void listarTopicos(){
+
+    }
+
+    @PostMapping
+    public void crearTopico(){
+
+    }
+
+    @PutMapping
+    public void actualizarTopicos(){
+
+    }
+
+    @DeleteMapping
+    public void eliminarTopicos(){
+
+    }
+
+}
